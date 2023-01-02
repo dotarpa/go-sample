54a3d4f9a88f73a01c5479573aa1a46c1d32f3cf
dotarpa <84436393+dotarpa@users.noreply.github.com>
2023-01-03 01:00:56 +0900

    first commit

---
+++ qp_enc/qpenc.go
@@ -0,0 +1,69 @@
+// https://github.com/prometheus/alertmanager/blob/main/notify/email/email.go
+// Just looking into mime/quotedprintable.
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"mime/multipart"
+	"mime/quotedprintable"
+	"net/textproto"
+	"strings"
+)
+
+func main() {
+	multipartBuffer := &bytes.Buffer{}
+	multipartWriter := multipart.NewWriter(multipartBuffer)
+	boundary := multipartWriter.Boundary()
+
+	w, err := multipartWriter.CreatePart(textproto.MIMEHeader{
+		"Content-Transfer-Encoding": {"quoted-printable"},
+		"Content-Type":              {"text/plain; charset=UTF-8"},
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body := "Hello, 世界: RFC2822で規定されている78文字以上のメッセージをうまいこと分割するmime/quotedprintableの使い方を調べてるだけ"
+
+	qw := quotedprintable.NewWriter(w)
+	_, err = qw.Write([]byte(body))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = qw.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = multipartWriter.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("boundary: %v\n", boundary)
+	fmt.Println(multipartBuffer.String())
+
+	dec_body := strings.NewReader(multipartBuffer.String())
+	multipartReader := multipart.NewReader(dec_body, boundary)
+	for {
+		p, err := multipartReader.NextPart()
+		if err == io.EOF {
+			return
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		slurp, err := io.ReadAll(p)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(slurp))
+	}
+}
